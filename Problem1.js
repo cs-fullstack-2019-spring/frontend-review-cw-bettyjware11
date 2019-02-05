eaf4a4b9b8ad593a3aaa88816376b16be6686e3b
student <bettyjcawthon@gmail.com>
2019-02-05 16:50:29 -0600

    morn cw

--- Problem1.js
+++ Problem1.js
@@ -1,11 +1,9 @@
 // ask the user if they want to continue being asked if they want to quit...
 
-// var userInput2 = prompt("Do you want to quit?");
-while(userInput1!='n');
-var userInput1 = prompt("Do you want to quit?");
-if (userInput1 === 'n') {
-    break;
+var userInput = prompt("Do you want to quit?");
+var input = prompt("Do you want to quit?");
+while(input !== 'n')
+{
+    input = prompt("Do you want to quit?")
 }
-
-
 
