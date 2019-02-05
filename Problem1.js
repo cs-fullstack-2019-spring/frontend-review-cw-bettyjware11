3cced154b0cd248ab2dc753c12cee651d83b947d
student <bettyjcawthon@gmail.com>
2019-02-05 12:55:09 -0600

    morn cw frontend rev

---
+++ Problem1.js
@@ -0,0 +1,11 @@
+// ask the user if they want to continue being asked if they want to quit...
+
+// var userInput2 = prompt("Do you want to quit?");
+while(userInput1!='n');
+var userInput1 = prompt("Do you want to quit?");
+if (userInput1 === 'n') {
+    break;
+}
+
+
+
